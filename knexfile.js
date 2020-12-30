819bafae6d1e31d41f22bf9833e10dd1155cf9bd
Mario Hoyos <mariohoyos92@gmail.com>
2020-12-29 16:14:07 -0800

    fix: can connect to db on prod

--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,13 @@ module.exports = {
   },
   production: {
     client: "postgresql",
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DATABASE,
+      ssl: { rejectUnauthorized: false },
+    },
     migrations: {
       tableName: "knex_migrations",
     },
